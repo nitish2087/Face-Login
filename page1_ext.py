31aa1c71b8de1d46ca6987bbc55d84551fed27e1
nitish2087 <vastav2087@gmail.com>
2019-12-26 18:50:53 +0530

    Used for collecting face data

---
+++ page1_ext.py
@@ -0,0 +1,78 @@
+import os
+import cv2
+face_cascade = cv2.CascadeClassifier('/home/nitish/Desktop/project/s_w/opencv-4.1.2/data/haarcascades/haarcascade_frontalface_default.xml')
+def face_extractor(img):
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #to convert into gray scale images
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+    #return faces
+    print(faces)
+
+    if faces is():
+        return None
+
+    for(x,y,w,h) in faces:
+        cropped_face = img[y:y+h, x:x+w]
+
+ +   return cropped_face #to crop faces
+
+cam = cv2.VideoCapture(0)
+#detector = cv2.CascadeClassifier('/home/nitish/Desktop/project/s_w/opencv-4.1.2/data/haarcascades/haarcascade_frontalface_default.xml')
+
+Id = input('enter your id: ')
+os.mkdir("/home/nitish/Desktop/project/faces/" + Id)
+count = 0
+
+while True:
+
+    ret, frame = cam.read()
+    if face_extractor(frame) is not None:
+        count += 1
+        #face = face_extractor(frame)
+        face = cv2.resize(face_extractor(frame), (250, 250))
+        face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+
+        #file_name_path = '/home/nitish/Desktop/project/faces/user'+str(count)+'.jpg'
+        file_name_path="/home/nitish/Desktop/project/faces/" + Id + "/" + Id + str(count) + ".jpg"
+        cv2.imwrite(file_name_path, face)
+
+        cv2.putText(face, str(count), (50,50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+        cv2.imshow('Face Detector', face)
+    else:
+        print("Face not Found")
+        pass
+
+#while (True):
+#   ret, img = cam.read()
+#   gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#   faces = detector.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+#    for (x, y, w, h) in faces:
+#        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+#        count += 1
+#        face = cv2.resize(face_extractor(frame), (250, 250))
+#        face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+#        # cv2.imwrite("images/user/."+Id+'.'+str(sampleNum)+".jpg",gray[y:y+h,x:x+w])
+#        cv2.imwrite("/home/nitish/Desktop/project/faces/" + Id + "/" + Id + str(sampleNum) + ".jpg", gray[y:y + h, x:x + w])
+
+#    cv2.imshow('frame', img)
+    if cv2.waitKey(1)==13 or count==200:
+        break
+
+cam.release()
+cv\
+
+
+    .destroyAllWindows()
+print('Collecting Samples Complete!!!')
+
+
+
+
+
+
+
+
+
+
+
+
