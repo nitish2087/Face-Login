9b75a9f497277c9507aa3ad3c604865155d634b8
nitish2087 <vastav2087@gmail.com>
2019-12-26 18:57:23 +0530

    Used for collecting face data

--- page1_ext.py
+++ page1_ext.py
@@ -17,7 +17,6 @@ def face_extractor(img):
  +   return cropped_face #to crop faces
 
 cam = cv2.VideoCapture(0)
-#detector = cv2.CascadeClassifier('/home/nitish/Desktop/project/s_w/opencv-4.1.2/data/haarcascades/haarcascade_frontalface_default.xml')
 
 Id = input('enter your id: ')
 os.mkdir("/home/nitish/Desktop/project/faces/" + Id)
@@ -28,11 +27,10 @@ while True:
     ret, frame = cam.read()
     if face_extractor(frame) is not None:
         count += 1
-        #face = face_extractor(frame)
+        
         face = cv2.resize(face_extractor(frame), (250, 250))
         face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
 
-        #file_name_path = '/home/nitish/Desktop/project/faces/user'+str(count)+'.jpg'
         file_name_path="/home/nitish/Desktop/project/faces/" + Id + "/" + Id + str(count) + ".jpg"
         cv2.imwrite(file_name_path, face)
 
@@ -42,27 +40,11 @@ while True:
         print("Face not Found")
         pass
 
-#while (True):
-#   ret, img = cam.read()
-#   gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#   faces = detector.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
-#    for (x, y, w, h) in faces:
-#        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-#        count += 1
-#        face = cv2.resize(face_extractor(frame), (250, 250))
-#        face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
-#        # cv2.imwrite("images/user/."+Id+'.'+str(sampleNum)+".jpg",gray[y:y+h,x:x+w])
-#        cv2.imwrite("/home/nitish/Desktop/project/faces/" + Id + "/" + Id + str(sampleNum) + ".jpg", gray[y:y + h, x:x + w])
-
-#    cv2.imshow('frame', img)
     if cv2.waitKey(1)==13 or count==200:
         break
 
 cam.release()
-cv\
-
-
-    .destroyAllWindows()
+cv.destroyAllWindows()
 print('Collecting Samples Complete!!!')
 
 
